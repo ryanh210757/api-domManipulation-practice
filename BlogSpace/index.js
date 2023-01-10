cd8db0638f09e42f0a4ec39bebfb7b5ec2220a31
ryanh210757 <kryanharris1@gmail.com>
2023-01-10 17:22:17 -0600

    update function

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -2,10 +2,16 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
         const newArray = data.slice(0,5)
-        const rootHTML = document.getElementById("root")
-        console.log(newArray)
-        for(let i=0; i < newArray.length; i++){
-            rootHTML.innerHTML += `The title to index ${i} is ${newArray[i].title}`
+        let html = ""
+        for(item of newArray){
+            html += `
+                <h3>${item.title}</h3>
+                <p>${item.body}</p>
+            `
         }
+
+        document.getElementById("root").innerHTML = html
+
+        
         
     })
