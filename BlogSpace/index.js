ee81e9a6b2baacb5a0c7f51f505eb44805f16f84
ryanh210757 <kryanharris1@gmail.com>
2023-01-11 14:41:09 -0600

    method post

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -11,7 +11,37 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         }
 
         document.getElementById("root").innerHTML = html
+})
 
+    document.getElementById("formData").addEventListener("submit", function(e) {
+        e.preventDefault()
         
-        
+        const title = document.getElementById("post-title").value
+        const body = document.getElementById("post-body").value
+        const data = {
+            title: title, 
+            body: body
+        }
+ 
+
+
+        const postMethod = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        }
+
+        //adds new entry to top of html
+        fetch("https://apis.scrimba.com/jsonplaceholder/posts", postMethod)
+            .then(res => res.json())
+            .then(data => {
+                document.getElementById("root").innerHTML = `
+                    <h3>${data.title}</h3>
+                    <p>${data.body}</p>
+                    ${document.getElementById("root").innerHTML}
+                `
+            })
+  
     })
